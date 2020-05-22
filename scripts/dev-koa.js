add9b3d4891415281211acf795f6be5b82c65f25
tngxiaowu <454789898@qq.com>
2020-05-22 13:54:09 +0800

    学习资源

--- scripts/dev-koa.js
+++ scripts/dev-koa.js
@@ -3,8 +3,8 @@ const  path = require('path'),
     Koa = require('koa'),
     serve = require('koa-static'),
 
-const app = new Koa(); // 实例化koa
-const rootPath =  path.join(__dirname + '/../dist');
+const app = new Koa()； // 实例化koa
+const rootPath =  path.join(__dirname + '/../dist'); // 指定静态资源路径
 // 指定静态服务目录
 app.use(serve(rootPath));
 app.listen(3000);
